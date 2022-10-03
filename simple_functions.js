e0b8b37e1852828a174fbe5e60a9471ac86e558e
Leslit Molina <113367291+lmolina613@users.noreply.github.com>
2022-10-03 11:03:55 -0400

    favoritethings

---
+++ simple_functions.js
@@ -0,0 +1,12 @@
+function favoriteThings(){
+    console.log("Name: pupusas")
+    console.log("Favorite Dog: Benjamin")
+    console.log("Favorite Band: The Neighbourhood")
+}
+function whyImHere(){
+     console.log("I joined this program to find a career that i can thrive and still be creative")
+}
+function favoritePlace(){
+    console.log("One of my favorite places to visit is LA because i get to see my family there and visit alot of places")
+}
+favoriteThings(); whyImHere(); favoritePlace();
